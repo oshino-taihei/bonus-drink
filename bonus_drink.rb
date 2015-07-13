85a08bd64ff0ee788d7a265e664e6d04e7c0c262
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-07-14 06:33:49 +0900

    refactoring

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,8 +1,8 @@
 class BonusDrink
+  BONUS = 3
   def self.total_count_for(amount)
-    return amount if amount < 3
-    bonus = amount / 3
-    rest = amount % 3
-    return amount - rest + self.total_count_for(bonus + rest)
+    return amount if amount < BONUS
+    bonus, rest = amount.divmod(BONUS)
+    return amount + total_count_for(bonus + rest) - rest
   end
 end
