0b42d00f0a7689f070c9e9c3c83e88bb3ac2ba70
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-07-14 06:12:44 +0900

    solve

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,8 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    return amount if amount < 3
+    bonus = amount / 3
+    rest = amount % 3
+    return amount - rest + self.total_count_for(bonus + rest)
   end
 end
